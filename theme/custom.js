77d04f53d34e420eed84363bbc4d4297d4486946
satori1995 <132636720+satori1995@users.noreply.github.com>
2024-12-10 10:20:33 +0800

    Update custom.js

--- theme/custom.js
+++ theme/custom.js
@@ -59,5 +59,9 @@ window.MathJax = {
     displayMath: [['$$', '$$'], ['\\[', '\\]']],
     processEscapes: true,
     processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
   }
 };
